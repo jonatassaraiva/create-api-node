b4cce3ff4f63c50e06e89b888ad0bbf9909f5113
Jonatas Saraiva <jonatas.saraiva@gmail.com>
2015-12-06 18:33:26 -0200

    Implementing Patch and Delete

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -26,25 +26,24 @@ var routes = function (Book) {
 			});
 		});
 
-	bookRouter.use('/:bookId', function(req, res, next){
+	bookRouter.use('/:bookId', function (req, res, next) {
 		Book.findById(req.params.bookId, function (err, book) {
-				if (err)
-					res.status(500).send(err);
-				else if (book)
-				{
-					req.book = book;
-					next();
-				}
-				else
-					res.status(404).send('Book not found');
-			});
+			if (err)
+				res.status(500).send(err);
+			else if (book) {
+				req.book = book;
+				next();
+			}
+			else
+				res.status(404).send('Book not found');
+		});
 	});
 
 	bookRouter.route('/:bookId')
 		.get(function (req, res) {
-			res.json(req.book);		
+			res.json(req.book);
 		})
-		.put(function(req, res){
+		.put(function (req, res) {
 			Book.findById(req.params.bookId, function (err, book) {
 				if (err)
 					res.status(500).send(err);
@@ -53,12 +52,37 @@ var routes = function (Book) {
 					req.book.author = req.body.author;
 					req.book.genre = req.body.genre;
 					req.book.read = req.body.read;
-					req.book.save();
 					
-					res.json(req.book);
+					req.book.save(function (err) {
+						if (err)
+							res.status(500).send(err);
+						else
+							res.json(req.book);
+					});
 				}
 			});
-			
+		})
+		.patch(function (req, res) {
+			if (req.body._id)
+				delete req.body._id;
+
+			for (var prop in req.body)
+				req.book[prop] = req.body[prop];
+
+			req.book.save(function (err) {
+				if (err)
+					res.status(500).send(err);
+				else
+					res.json(req.book);
+			});
+		})
+		.delete(function(req, res){
+			req.book.remove(function(err){
+				if (err)
+					res.status(500).send(err);
+				else
+					res.status(204).send('Book removed');
+			});
 		});
 
 	return bookRouter;
