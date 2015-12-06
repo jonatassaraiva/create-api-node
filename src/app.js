00ea36480a0126e59fbd22b328bfb57026ad196d
Jonatas Saraiva <jonatas.saraiva@gmail.com>
2015-12-06 15:44:09 -0200

    Implementing HTTP GET

--- src/app.js
+++ src/app.js
@@ -5,10 +5,23 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+	.get(function (req, rep) {
+		var responseJson = {
+			hellow: 'This is json response'
+		};
+		
+		rep.json(responseJson);
+	});
+
+app.use('/api', bookRouter);
+
+app.get('/', function (req, res) {
 	res.send('Welcome to API');
 });
 
-app.listen(port, function(){
-	console.log('Gulp is running API on localhost:'+port);
+app.listen(port, function () {
+	console.log('Gulp is running API on localhost:' + port);
 });
