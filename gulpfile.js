c2e204014dcfe0141922c483990597619f6c8b02
Jonatas Saraiva <jonatas.saraiva@gmail.com>
2015-12-07 23:25:09 -0200

    Gulp env and supertest

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
 	gulpNodemon = require('gulp-nodemon'),
-	gulpMocha = require('gulp-mocha');
+	gulpMocha = require('gulp-mocha'),
+	gulpEnv = require('gulp-env'),
+	supertest = require('supertest');
 
 gulp.task('run-nodemon', function () {
 	gulpNodemon({
@@ -16,6 +18,7 @@ gulp.task('run-nodemon', function () {
 });
 
 gulp.task('run-tests', function () {
+	gulpEnv({ vars: { ENV: 'IntegrationTests' } });
 	gulp.src('src/tests/*.js', { read: false })
         .pipe(gulpMocha({ reporter: 'spec' }));
 });
